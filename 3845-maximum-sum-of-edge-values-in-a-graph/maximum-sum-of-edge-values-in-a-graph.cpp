1d1c5ad189200c52163f8179490593147dd3c0af
Uday bisone <155158921+Udaybisone@users.noreply.github.com>
2025-05-23 00:23:04 +0530

    Time: 392 ms (26.29%) | Memory: 371.3 MB (10.80%) - LeetSync

--- 3845-maximum-sum-of-edge-values-in-a-graph/maximum-sum-of-edge-values-in-a-graph.cpp
+++ 3845-maximum-sum-of-edge-values-in-a-graph/maximum-sum-of-edge-values-in-a-graph.cpp
@@ -1,13 +1,5 @@
 class Solution {
-    bool iscycle(int node,int par,vector<vector<int>>&adj,vector<int>&vis){
-        vis[node] = 1;
-        for(auto child:adj[node]){
-            if(!vis[child]){
-                if(iscycle(child,node,adj,vis)) return true;
-            }else if(child != par) return true;
-        }
-        return false;
-    }   
+    
 public:
     long long maxScore(int n, vector<vector<int>>& edges) {
         vector<vector<int>>adj(n);
@@ -20,7 +12,7 @@ public:
         if(n>1) ans += ((n*1LL*(n-1))*1LL);
         for(long long i=n-1; i>2; i-=2) ans += (i*(i-2));
         vector<int>vis(n,0);
-        if(iscycle(0,-1,adj,vis)) ans += 2;
+        if(n==edges.size()) ans += 2;
         return ans;
     }
 };
