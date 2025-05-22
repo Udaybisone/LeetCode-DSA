987242cadbb0b71afad71f6be5d1b7b89b468152
Uday bisone <155158921+Udaybisone@users.noreply.github.com>
2025-05-22 22:38:56 +0530

    Time: 317 ms (71.50%) | Memory: 185.6 MB (23.14%) - LeetSync

--- 3837-grid-teleportation-traversal/grid-teleportation-traversal.cpp
+++ 3837-grid-teleportation-traversal/grid-teleportation-traversal.cpp
@@ -7,59 +7,58 @@ public:
         int n = mat.size(), m = mat[0].size();
         if (mat[0][0] == '#') return -1;
 
-        // Collect teleport positions for each letter
+        // Teleport positions per letter
         vector<vector<pair<int,int>>> telp(26);
-        for (int i = 0; i < n; ++i) {
+        for (int i = 0; i < n; ++i)
             for (int j = 0; j < m; ++j) {
                 char c = mat[i][j];
-                if (c >= 'A' && c <= 'Z') {
+                if (c >= 'A' && c <= 'Z')
                     telp[c - 'A'].emplace_back(i, j);
-                }
             }
-        }
 
-        // dist and visited masks
-        vector<vector<int>> dist(n, vector<int>(m, INT_MAX));
-        // priority queue of (distance, row, col, used_mask)
+        // 0-1 BFS: state = (row, col, mask)
         using State = tuple<int,int,int,int>;
-        priority_queue<State, vector<State>, greater<State>> pq;
+        int maxMask = 1 << 26;
+        // dist[row][col][mask] large, but we use 2D dist and clear teleports once
+        vector<vector<int>> dist(n, vector<int>(m, INT_MAX));
+        deque<State> dq;
 
         dist[0][0] = 0;
-        pq.emplace(0, 0, 0, 0); 
+        dq.emplace_front(0, 0, 0, 0);
 
-        while (!pq.empty()) {
-            auto [d, r, c, mask] = pq.top();
-            pq.pop();
-
+        while (!dq.empty()) {
+            auto [r, c, mask, d] = dq.front();
+            dq.pop_front();
             if (d > dist[r][c]) continue;
             if (r == n - 1 && c == m - 1) return d;
 
+            // Teleportation (cost 0)
             if (mat[r][c] >= 'A' && mat[r][c] <= 'Z') {
                 int idx = mat[r][c] - 'A';
                 if (!(mask & (1 << idx))) {
                     int newMask = mask | (1 << idx);
-                    for (auto& p : telp[idx]) {
-                        int nr = p.first, nc = p.second;
+                    for (auto& [nr, nc] : telp[idx]) {
                         if (d < dist[nr][nc]) {
                             dist[nr][nc] = d;
-                            pq.emplace(d, nr, nc, newMask);
+                            dq.emplace_front(nr, nc, newMask, d);
                         }
                     }
+                    // Prevent re-teleporting on this letter
+                    telp[idx].clear();
                 }
             }
 
-            // Standard 4-direction moves
+            // Adjacent moves (cost 1)
             for (int i = 0; i < 4; ++i) {
                 int nr = r + delr[i], nc = c + delc[i];
                 if (nr < 0 || nr >= n || nc < 0 || nc >= m) continue;
                 if (mat[nr][nc] == '#') continue;
                 if (d + 1 < dist[nr][nc]) {
                     dist[nr][nc] = d + 1;
-                    pq.emplace(d + 1, nr, nc, mask);
+                    dq.emplace_back(nr, nc, mask, d + 1);
                 }
             }
         }
-
         return -1;
     }
 };
