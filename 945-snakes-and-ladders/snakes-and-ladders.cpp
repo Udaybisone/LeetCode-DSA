3dccf953bcc314487bd7b1847f02343efa39d1db
Uday bisone <155158921+Udaybisone@users.noreply.github.com>
2025-05-31 10:54:33 +0530

    Time: 3 ms (63.03%) | Memory: 18.2 MB (23.33%) - LeetSync

--- 945-snakes-and-ladders/snakes-and-ladders.cpp
+++ 945-snakes-and-ladders/snakes-and-ladders.cpp
@@ -1,6 +1,6 @@
 class Solution {
     pair<int, int> rowcol(int cnt, int n) {
-        cnt--; // Convert to 0-based index
+        cnt--;
         int row = n - 1 - (cnt / n);
         int col = cnt % n;
         if ((n - 1 - row) % 2 == 1) {
@@ -12,33 +12,33 @@ class Solution {
 public:
     int snakesAndLadders(vector<vector<int>>& grid) {
         int n = grid.size();
-        priority_queue<tuple<int, int>, vector<tuple<int, int>>, greater<>> pq;
-        vector<int> dist(n * n + 1, 1e9); // 1D distance array since we care about cell numbers
-        dist[1] = 0;
-        pq.push({0, 1}); // {moves, cell number}
-
-        while (!pq.empty()) {
-            auto [moves, cell] = pq.top();
+        // moves,cnt;
+        priority_queue<tuple<int,int>,vector<tuple<int,int>>,greater<tuple<int,int>>>pq;
+        vector<vector<int>>dist(n,vector<int>(n,1e9));
+        dist[n-1][0] = 0;
+        pq.push({0,1});
+        
+        while(!pq.empty()){
+            auto [move,cnt] = pq.top();
             pq.pop();
+            
+            if(cnt==(n*n)) return move;
 
-            if (cell == n * n) return moves;
-
-            for (int i = 1; i <= 6; i++) {
-                int next = cell + i;
-                if (next > n * n) continue;
-
-                auto [r, c] = rowcol(next, n);
-                if (grid[r][c] != -1) {
-                    next = grid[r][c]; // snake or ladder destination
-                }
-
-                if (dist[next] > moves + 1) {
-                    dist[next] = moves + 1;
-                    pq.push({moves + 1, next});
+            for(int i=1; i<=6; i++){
+                int next = cnt + i;
+                if(next > n*n) continue;
+                
+                auto [r,c] = rowcol(next,n);
+                if(grid[r][c]!=-1) next = grid[r][c];
+                auto k = rowcol(next,n);
+                r = k.first;
+                c = k.second;
+                if(dist[r][c]>move+1){
+                    dist[r][c] = move+1;
+                    pq.push({move+1,next});
                 }
             }
         }
-
-        return -1; // unreachable
+        return -1;
     }
 };
