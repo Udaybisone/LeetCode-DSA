fff83ca9d87c253c0b11186c8e3f1d35355d4526
Uday bisone <155158921+Udaybisone@users.noreply.github.com>
2025-06-04 00:21:42 +0530

    Time: 42 ms (47.52%) | Memory: 17.8 MB (57.55%) - LeetSync

--- 877-shortest-path-visiting-all-nodes/shortest-path-visiting-all-nodes.cpp
+++ 877-shortest-path-visiting-all-nodes/shortest-path-visiting-all-nodes.cpp
@@ -1,11 +1,8 @@
 class Solution {
+    vector<vector<int>>dist;
+    int goal=0;
     int f(int start,vector<vector<int>>& adj){
         int n = adj.size();
-        int goal = 0;
-        for(int i=0; i<n; i++) goal += (1<<i);
-        // node,bitmask
-        vector<vector<int>>dist(n+1,vector<int>(goal+1,1e9));
-        // dis,node,bitmask
         priority_queue<tuple<int,int,int>,vector<tuple<int,int,int>>,greater<tuple<int,int,int>>>pq;
         pq.push({0,start,(1<<start)});
         dist[start][(1<<start)] = 0;
@@ -29,7 +26,8 @@ class Solution {
 public:
     int shortestPathLength(vector<vector<int>>& graph) {
         int n = graph.size();
-
+        for(int i=0; i<n; i++) goal += (1<<i);
+        dist.resize(n+1,vector<int>(goal+1,1e9));
         int ans = 1e9;
         for(int node=0;node<n; node++){
             int dist = f(node,graph);
