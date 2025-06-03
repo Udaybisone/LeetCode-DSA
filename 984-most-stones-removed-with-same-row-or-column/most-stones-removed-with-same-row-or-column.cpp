9bd631be046d3b8a302cd8fc24fe75abd87b3e53
Uday bisone <155158921+Udaybisone@users.noreply.github.com>
2025-06-03 14:16:01 +0530

    Time: 34 ms (36.28%) | Memory: 35.7 MB (5.19%) - LeetSync

--- 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.cpp
+++ 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.cpp
@@ -1,46 +1,34 @@
 class Solution {
-    set<pair<int,int>> vis;
-    map<int, vector<pair<int,int>>> rowMap, colMap;
-    map<pair<int,int>, set<pair<int,int>>> adj;
+    map<int, set<int>> rowAdj, colAdj;
+    set<pair<int, int>> vis;
 
-    void dfs(pair<int,int> node) {
-        vis.insert(node);
-        for(auto& child : adj[node]) {
-            if(vis.find(child) == vis.end()) {
-                dfs(child);
-            }
+    void dfs(int x, int y) {
+        if (vis.count({x, y})) return;
+        vis.insert({x, y});
+        for (int nx : rowAdj[x]) {
+            if (!vis.count({x, nx}))
+                dfs(x, nx);
+        }
+        for (int ny : colAdj[y]) {
+            if (!vis.count({ny, y}))
+                dfs(ny, y);
         }
     }
 
 public:
     int removeStones(vector<vector<int>>& stones) {
-        // Build maps of rows and columns
-        for(auto& stone : stones) {
-            int x = stone[0], y = stone[1];
-            rowMap[x].emplace_back(x, y);
-            colMap[y].emplace_back(x, y);
+        // Build adjacency by row and column
+        for (auto& s : stones) {
+            int x = s[0], y = s[1];
+            rowAdj[x].insert(y);
+            colAdj[y].insert(x);
         }
 
-        // Build the adjacency list
-        for(auto& [x, vec] : rowMap) {
-            for(size_t i = 1; i < vec.size(); ++i) {
-                adj[vec[0]].insert(vec[i]);
-                adj[vec[i]].insert(vec[0]);
-            }
-        }
-        for(auto& [y, vec] : colMap) {
-            for(size_t i = 1; i < vec.size(); ++i) {
-                adj[vec[0]].insert(vec[i]);
-                adj[vec[i]].insert(vec[0]);
-            }
-        }
-
-        // DFS to count components
         int components = 0;
-        for(auto& stone : stones) {
-            pair<int,int> p = {stone[0], stone[1]};
-            if(vis.find(p) == vis.end()) {
-                dfs(p);
+        for (auto& s : stones) {
+            int x = s[0], y = s[1];
+            if (!vis.count({x, y})) {
+                dfs(x, y);
                 components++;
             }
         }
