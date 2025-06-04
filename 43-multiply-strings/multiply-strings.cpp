458cda19030cd191e2b2c68a95be5a4fa4bf46cb
Uday bisone <155158921+Udaybisone@users.noreply.github.com>
2025-06-04 22:47:19 +0530

    Time: 9 ms (15.82%) | Memory: 11.7 MB (15.64%) - LeetSync

--- 43-multiply-strings/multiply-strings.cpp
+++ 43-multiply-strings/multiply-strings.cpp
@@ -1,30 +1,27 @@
 class Solution {
 public:
     string multiply(string num1, string num2) {
-        if (num1 == "0" || num2 == "0") return "0";
+        if(num1=="0" || num2=="0") return "0";
+        int n = num1.size(),m = num2.size();
+        vector<int>res(n+m,0);
+        string carry = "0";
+        for(int i=n-1; i>=0; i--){
+            for(int j=m-1; j>=0; j--){
+                int mul = (num1[i]-'0')*(num2[j]-'0') + stoi(carry);
+                int sum = mul + res[i+j+1];
 
-        int n = num1.size(), m = num2.size();
-        vector<int> result(n + m, 0);
-
-        // Multiply each digit
-        for (int i = n - 1; i >= 0; i--) {
-            for (int j = m - 1; j >= 0; j--) {
-                int mul = (num1[i] - '0') * (num2[j] - '0');
-                int sum = mul + result[i + j + 1];
-
-                result[i + j + 1] = sum % 10;          // Store unit digit
-                result[i + j] += sum / 10;             // Carry to next left
+                res[i+j+1] = sum%10;
+                res[i+j] += sum/10;
             }
         }
-
-        // Convert result vector to string
-        string product = "";
-        for (int num : result) {
-            if (!(product.empty() && num == 0)) {
-                product += (num + '0');
-            }
+        n += m;
+        string ans = "";
+        int i = 0;
+        while(res[i]==0) i++;
+        while(i<n){
+            ans += to_string(res[i]);
+            i++;
         }
-
-        return product.empty() ? "0" : product;
+        return ans;
     }
 };
