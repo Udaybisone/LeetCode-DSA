2997fccdd11388104415c7fbe7b8e7328327128a
Uday bisone <155158921+Udaybisone@users.noreply.github.com>
2025-06-04 21:59:30 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (81.68%) - LeetSync

--- 43-multiply-strings/multiply-strings.cpp
+++ 43-multiply-strings/multiply-strings.cpp
@@ -1,58 +1,30 @@
 class Solution {
 public:
     string multiply(string num1, string num2) {
+        if (num1 == "0" || num2 == "0") return "0";
+
         int n = num1.size(), m = num2.size();
-        string sum = "";
-        int cnt = 0;
-        for(int i = m - 1; i >= 0; i--) {
-            string pr = "", carry = "0";
-            for(int j = n - 1; j >= 0; j--) {
-                int k = (num1[j] - '0') * (num2[i] - '0') + stoi(carry);
-                string s = to_string(k);
-                if(s.size() == 1) {
-                    pr = s + pr;
-                    carry = "0";
-                } else {
-                    pr = s.back() + pr;
-                    carry = s[0];
-                }
-            }
-            if(carry != "0") pr = carry + pr;
-            string temp(cnt, '0');
-            pr += temp;
-            cnt++;
+        vector<int> result(n + m, 0);
 
-            if(sum == "") {
-                sum = pr;
-            } else {
-                string newsum = "";
-                carry = "0";
-                int n1 = pr.size() - 1;
-                int n2 = sum.size() - 1;
+        // Multiply each digit
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = m - 1; j >= 0; j--) {
+                int mul = (num1[i] - '0') * (num2[j] - '0');
+                int sum = mul + result[i + j + 1];
 
-                while(n1 >= 0 || n2 >= 0 || carry != "0") {
-                    int digit1 = (n1 >= 0) ? pr[n1] - '0' : 0;
-                    int digit2 = (n2 >= 0) ? sum[n2] - '0' : 0;
-                    int k = digit1 + digit2 + stoi(carry);
-                    string s = to_string(k);
-                    if(s.size() == 1) {
-                        newsum = s + newsum;
-                        carry = "0";
-                    } else {
-                        newsum = s.back() + newsum;
-                        carry = s.substr(0, s.size() - 1);
-                    }
-                    n1--;
-                    n2--;
-                }
-
-                sum = newsum;
+                result[i + j + 1] = sum % 10;          // Store unit digit
+                result[i + j] += sum / 10;             // Carry to next left
             }
         }
 
-        // Remove leading zeros
-        int i = 0;
-        while(i < sum.size() - 1 && sum[i] == '0') i++;
-        return sum.substr(i);
+        // Convert result vector to string
+        string product = "";
+        for (int num : result) {
+            if (!(product.empty() && num == 0)) {
+                product += (num + '0');
+            }
+        }
+
+        return product.empty() ? "0" : product;
     }
 };
