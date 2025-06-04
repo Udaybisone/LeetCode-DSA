b0f3d6351ce91f75cc3a3044d2abf5d501a7ea45
Uday bisone <155158921+Udaybisone@users.noreply.github.com>
2025-06-04 16:16:00 +0530

    Time: 99 ms (74.71%) | Memory: 95.7 MB (18.38%) - LeetSync

--- 464-can-i-win/can-i-win.cpp
+++ 464-can-i-win/can-i-win.cpp
@@ -1,31 +1,26 @@
 class Solution {
     int n;
-    unordered_map<int, bool> dp;
+    vector<int>dp;
+    int f(int sum,int bitmask){
+        if(dp[bitmask]!=-1) return dp[bitmask];
 
-    bool f(int sum, int bitmask) {
-        if (dp.count(bitmask)) return dp[bitmask];
-
-        for (int i = 1; i <= n; i++) {
-            if (!((1 << i) & bitmask)) {
-                if (i >= sum) {
-                    return dp[bitmask] = true;  // current player wins immediately
-                }
-                if (!f(sum - i, bitmask | (1 << i))) {
-                    return dp[bitmask] = true;  // current player can force a win
-                }
-            }
+        for(int i=1; i<=n; i++){
+           if(!(bitmask&(1<<i))){
+                if(sum<=i) return dp[bitmask] = true;
+                if(!f(sum-i,bitmask | (1<<i))) return dp[bitmask] = true;
+           }
         }
-        return dp[bitmask] = false;  // no winning move found
-    }
 
+        return dp[bitmask] = false;
+    }
 public:
     bool canIWin(int n, int sum) {
         this->n = n;
-
-        int maxPossible = (n * (n + 1)) / 2;
-        if (sum <= 0) return true;
-        if (maxPossible < sum) return false;
-
-        return f(sum, 0);  // initial state: sum needed, no numbers used
+        int maxpossible = (n*(n+1))/2;
+        if(maxpossible < sum) return false;
+        if(sum<=0) return true;
+        int bitmask = 0;
+        dp.resize(1<<(n+1),-1);
+        return f(sum,bitmask);
     }
 };
