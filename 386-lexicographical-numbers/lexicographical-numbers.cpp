24e46f5440595bc6a6b227188ba985b69b2e8914
Uday bisone <155158921+Udaybisone@users.noreply.github.com>
2025-06-09 09:39:55 +0530

    Time: 3 ms (47.04%) | Memory: 14 MB (70.17%) - LeetSync

--- 386-lexicographical-numbers/lexicographical-numbers.cpp
+++ 386-lexicographical-numbers/lexicographical-numbers.cpp
@@ -1,73 +1,26 @@
-class TrieNode{
-    public:
-    char data;
-    vector<TrieNode*> children;
-    bool isTerminal;
-    TrieNode(char ch){
-        data = ch;
-        children.resize(10);
-        isTerminal = false;
-
-        for(int i=0;i<10;i++){
-            children[i] = NULL;
-        }
-    }
-};
-
 class Solution {
 public:
-    void insert_func(TrieNode* root,string word){
-            if(word.length()==0){
-                root->isTerminal = true;
-                return;
-            }
-            char ch = word[0];
-            int index = ch - '0';
-            TrieNode* child;
-
-            if(root->children[index] == NULL){
-                //absent
-                child = new TrieNode(ch);
-                root->children[index] = child; 
-            }
-            else{
-                //present
-                child = root->children[index];
-            }
-            insert_func(child,word.substr(1));
-    }
-    void search(TrieNode* root,vector<int> &ans,string s){
-        if(root->isTerminal == true){
-            int d = stoi(s);
-            ans.push_back(d);
-            
-        }
-        for(int i=0;i<root->children.size();i++){
-            if(root->children[i] != NULL){
-                search(root->children[i],ans,s+root->children[i]->data);
-            }
-        }
-    }
     vector<int> lexicalOrder(int n) {
+        int temp = 0;
+        int num = 1;
+        vector<int>ans;
+        ans.push_back(num);
 
-        //my own code
-
-        TrieNode* root = new TrieNode('\0');
-        
-        for(int i=1;i<=n;i++){
-            string str = to_string(i);
-            insert_func(root,str);
+        while(true){
+            if(num*10 + temp <=n){
+                num = num*10 + temp;
+                temp = 0;
+                ans.push_back(num);
+            }else{
+                while(num%10==9){
+                    num /= 10;
+                }
+                if(num==0) break;
+                temp = num%10;
+                temp++;
+                num /= 10;
+            }
         }
-
-        vector<int> ans;
-        search(root,ans,"");
         return ans;
     }
 };
-
-auto init = [](){
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
-    return 'c';
-}();
