e7bc1919c6a2c6c6d48d40e7ea3fa7818c7614d0
Uday bisone <155158921+Udaybisone@users.noreply.github.com>
2025-08-30 01:26:10 +0530

    Time: 278 ms (37.24%) | Memory: 180.2 MB (75.53%) - LeetSync

--- 1891-count-pairs-of-nodes/count-pairs-of-nodes.cpp
+++ 1891-count-pairs-of-nodes/count-pairs-of-nodes.cpp
@@ -1,64 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
-    vector<int> countPairs(int n, vector<vector<int>>& edges, vector<int>& q) {
-        vector<int> d(n, 0);
-        vector<vector<int>> adj(n);
-        unordered_map<long long, int> edgeCount;
+    vector<int> countPairs(int n, vector<vector<int>>& edges, vector<int>& queries) {
+        vector<int> deg(n+1,0);
+        map<pair<int,int>,int> cnt;
 
-        for (auto& it : edges) {
-            int u = it[0] - 1;
-            int v = it[1] - 1;
-            d[u]++;
-            d[v]++;
-            adj[u].push_back(v);
-            adj[v].push_back(u);
-
-            if (u > v) swap(u, v); // ensure (u,v) order
-            edgeCount[1LL * u * n + v]++; // encode pair uniquely
+        // 1. store degrees and edge counts
+        for (auto &e: edges) {
+            int u=e[0], v=e[1];
+            deg[u]++; deg[v]++;
+            if (u>v) swap(u,v);
+            cnt[{u,v}]++;
         }
 
-        vector<pair<int, int>> dg;
-        for (int i = 0; i < n; i++) {
-            dg.push_back({d[i], i});
-        }
-
-        sort(dg.begin(), dg.end());
-
-        vector<int> degOnly;
-        for (auto& p : dg) degOnly.push_back(p.first);
-
-        unordered_map<int, int> mp;
-        for (int i = 0; i < n; i++) {
-            mp[dg[i].second] = i;
-        }
+        // 2. sort degrees
+        vector<int> sorted(deg.begin()+1, deg.end());
+        sort(sorted.begin(), sorted.end());
 
         vector<int> ans;
 
-        for (int query : q) {
-            int total = 0;
-            int i = 0, j = n - 1;
-            // Two-pointer approach to count pairs (i < j) where deg[i] + deg[j] > query
-            while (i < j) {
-                if (degOnly[i] + degOnly[j] > query) {
-                    total += (j - i);
-                    j--;
-                } else {
-                    i++;
-                }
+        for (int q: queries) {
+            long long total=0;
+
+            // 3. count pairs using lower_bound
+            for (int i=0;i<n;i++) {
+                int need = q - sorted[i];
+                auto it = upper_bound(sorted.begin()+i+1, sorted.end(), need);
+                total += (sorted.end()-it);
             }
 
-            // Subtract overcounted pairs due to common edges
-            for (auto& it : edgeCount) {
-                int key = it.first;
-                int u = key / n;
-                int v = key % n;
-                int common = it.second;
-                if (d[u] + d[v] > query && d[u] + d[v] - common <= query) {
-                    total--;
-                }
+            // 4. correct with multi-edges
+            for (auto &p: cnt) {
+                int u=p.first.first, v=p.first.second, c=p.second;
+                if (deg[u]+deg[v] > q && deg[u]+deg[v]-c <= q) total--;
             }
 
-            ans.push_back(total);
+            ans.push_back((int)total);
         }
 
         return ans;
