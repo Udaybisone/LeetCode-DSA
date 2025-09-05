2a53094b7bb1b6fe569cfa775a6911bd4f40ad50
Uday bisone <155158921+Udaybisone@users.noreply.github.com>
2025-09-05 16:09:10 +0530

    Time: 32 ms (32.77%) | Memory: 139.1 MB (59.86%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -1,33 +1,22 @@
-#include <deque>
-#include <vector>
-using namespace std;
-
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        deque<int> dq; // stores indices of useful elements
-        vector<int> res;
-
-        for (int i = 0; i < nums.size(); i++) {
-            // 1. Remove elements that are out of this window
-            if (!dq.empty() && dq.front() <= i - k) {
-                dq.pop_front();
-            }
-
-            // 2. Remove all smaller elements from back
-            while (!dq.empty() && nums[dq.back()] < nums[i]) {
-                dq.pop_back();
-            }
-
-            // 3. Add current element index
-            dq.push_back(i);
-
-            // 4. The max element for valid windows
-            if (i >= k - 1) {
-                res.push_back(nums[dq.front()]);
-            }
+        deque<int>q;
+        int n = nums.size();
+        int r = 0;
+        while(r<k){
+            while(!q.empty() && q.back()<nums[r]) q.pop_back();
+            q.push_back(nums[r++]);
         }
-
-        return res;
+        vector<int>ans;
+        ans.push_back(q.front());
+        for(r=r; r<n; r++){
+            int l = r-k;
+            if(q.front()==nums[l]) q.pop_front();
+            while(!q.empty() && q.back()<nums[r]) q.pop_back();
+            q.push_back(nums[r]);
+            ans.push_back(q.front());
+        }
+        return ans;
     }
 };
