23ab13ac5d8721c8b0601e725d325b4ce2217a6a
Uday bisone <155158921+Udaybisone@users.noreply.github.com>
2025-09-07 11:04:23 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (83.69%) - LeetSync

---
+++ 1426-find-n-unique-integers-sum-up-to-zero/find-n-unique-integers-sum-up-to-zero.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> sumZero(int n) {
+        vector<int>ans;
+        for(int i=(0-n)/2; i<=n/2; i++){
+            if(!(n%2) && i==0) continue;
+            ans.push_back(i);
+        }
+        return ans;
+    }
+};
