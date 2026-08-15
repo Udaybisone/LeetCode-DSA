4f75e67c5a34e37b5372ee8a3be2d29fc2fdf20f
Uday bisone <155158921+Udaybisone@users.noreply.github.com>
2026-08-15 22:33:22 +0530

    Time: 7 ms (42.32%) | Memory: 14 MB (28.33%) - LeetSync

--- 2187-vowels-of-all-substrings/vowels-of-all-substrings.cpp
+++ 2187-vowels-of-all-substrings/vowels-of-all-substrings.cpp
@@ -1,18 +1,14 @@
 class Solution {
 public:
     long long countVowels(string word) {
-        long long sum = 0,ans = 0;
-        int i = 0;
+        long long sum = 0,ans = 0,i=0;
         for(auto c:word){
             if(c=='u' or c=='a' or c=='e' or c=='i' or c=='o'){
                 ans += (sum + i + 1);
                 sum = sum + i + 1;
-            }else{
-                ans += sum;
-            }
+            }else ans += sum;
             i++;
         }
-
         return ans;
     }
 };
